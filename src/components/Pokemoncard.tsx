1b38854cfbfdaab0805cad69f0fa984d033a3d49
BryanDev1 <bolangebryan@gmail.com>
2024-10-22 19:01:28 +0200

    compenents 2 exercice 3

--- src/components/Pokemoncard.tsx
+++ src/components/Pokemoncard.tsx
@@ -1,11 +1,25 @@
+const pokemonList = [
+  {
+    name: "Bulbausar",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+
+  {
+    name: "Mew",
+  },
+];
+
 function Pokemoncard() {
+  const pokemon = pokemonList[0];
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>Qu'est ce que c'est</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
