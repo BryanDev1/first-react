62360ab148c204c15adc36d08725177b810ee7d2
BryanDev1 <bolangebryan@gmail.com>
2024-10-24 12:23:04 +0200

    end exercices 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
+import { useState } from "react";
 import "./App.css";
 import MyTitle from "./components/Mytitle";
 import Pokemoncard from "./components/Pokemoncard";
 
 const pokemonList = [
   {
-    name: "Bulbausar",
+    name: "bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
-
   {
-    name: "Mew",
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
   },
 ];
 
 function App() {
+  const [Index, pokemonIndex] = useState(0);
+
+  const Previous = () => {
+    pokemonIndex(Index - 1);
+  };
+
+  const Next = () => {
+    pokemonIndex(Index + 1);
+  };
+
   return (
     <div>
       <MyTitle />
-      <Pokemoncard pokemon={pokemonList[0]} />
+      <Pokemoncard pokemon={pokemonList[Index]} />
+      <button type="button" onClick={Previous}>
+        Previous
+      </button>
+      <button type="button" onClick={Next}>
+        Next
+      </button>
     </div>
   );
 }
