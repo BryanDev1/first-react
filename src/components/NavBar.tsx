2a744f4582061b72cd4ba004a9fe4f1d978640b8
BryanDev1 <bolangebryan@gmail.com>
2024-10-26 14:49:04 +0200

    finish exercice 8

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,14 +12,22 @@ interface NavBarProps {
 function NavBar({ Index, PokemonIndex, pokemonList }: NavBarProps) {
   return (
     <>
-      <button type="button" onClick={() => PokemonIndex(Index - 1)}>
-        Previous
-      </button>
-      <button type="button" onClick={() => PokemonIndex(Index + 1)}>
-        Next
-      </button>
+      <p>
+        {pokemonList.map((listPokemon, index) => (
+          <button type="button" key={index} onClick={() => PokemonIndex(index)}>
+            {listPokemon.name}
+          </button>
+        ))}
+      </p>
     </>
   );
 }
 
 export default NavBar;
+
+/* <button type="button" onClick={() => PokemonIndex(Index - 1)}>
+Previous
+</button>
+<button type="button" onClick={() => PokemonIndex(Index + 1)}>
+Next
+</button> */
